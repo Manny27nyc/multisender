5b3bdbf9d0fb60f64713c1a1f93e427bea50cd72
Oleg Abrosimov <olegabrosimovnsk@gmail.com>
2022-03-02 18:06:44 +0700

    gas price API changed

--- src/getWeb3.js
+++ src/getWeb3.js
@@ -53,7 +53,7 @@ let getWeb3 = () => {
                 trustApiName = 'ropsten'
                 explorerUrl = 'https://ropsten.etherscan.io'
                 explorerAPIUrl = 'https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=%1$s&startblock=0&endblock=999999999&sort=desc&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
-                gasPriceAPIUrl = 'https://api-ropsten.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
+                gasPriceAPIUrl = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
                 currencyTicker = 'ETH'
                 currencyTickerName = 'Ether'
                 blockchainName = 'Ethereum'
@@ -64,7 +64,7 @@ let getWeb3 = () => {
                 trustApiName = 'rinkeby'
                 explorerUrl = 'https://rinkeby.etherscan.io'
                 explorerAPIUrl = 'https://api-rinkeby.etherscan.io/api?module=account&action=tokentx&address=%1$s&startblock=0&endblock=999999999&sort=desc&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
-                gasPriceAPIUrl = 'https://api-rinkeby.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
+                gasPriceAPIUrl = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
                 currencyTicker = 'ETH'
                 currencyTickerName = 'Ether'
                 blockchainName = 'Ethereum'
@@ -75,7 +75,7 @@ let getWeb3 = () => {
                 trustApiName = 'kovan'
                 explorerUrl = 'https://kovan.etherscan.io'
                 explorerAPIUrl = 'https://api-kovan.etherscan.io/api?module=account&action=tokentx&address=%1$s&startblock=0&endblock=999999999&sort=desc&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
-                gasPriceAPIUrl = 'https://api-kovan.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
+                gasPriceAPIUrl = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_ETHERSCAN_API_KEY"]
                 currencyTicker = 'ETH'
                 currencyTickerName = 'Ether'
                 blockchainName = 'Ethereum'
@@ -86,7 +86,7 @@ let getWeb3 = () => {
                 trustApiName = 'bsc'
                 explorerUrl = 'https://bscscan.com'
                 explorerAPIUrl = 'https://api.bscscan.com/api?module=account&action=tokentx&address=%1$s&startblock=0&endblock=999999999&sort=desc&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_BSCSCAN_API_KEY"]
-                gasPriceAPIUrl = null//'https://api.bscscan.com/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_BSCSCAN_API_KEY"]
+                gasPriceAPIUrl = 'https://api.bscscan.com/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_BSCSCAN_API_KEY"]
                 currencyTicker = 'BNB'
                 currencyTickerName = 'BNB'
                 blockchainName = 'Binance Smart Chain'
@@ -97,7 +97,7 @@ let getWeb3 = () => {
                 trustApiName = 'bsctest'
                 explorerUrl = 'https://testnet.bscscan.com'
                 explorerAPIUrl = 'https://api-testnet.bscscan.com/api?module=account&action=tokentx&address=%1$s&startblock=0&endblock=999999999&sort=desc&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_BSCSCAN_API_KEY"]
-                gasPriceAPIUrl = null//'https://api.bscscan.com/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_BSCSCAN_API_KEY"]
+                gasPriceAPIUrl = 'https://api.bscscan.com/api?module=gastracker&action=gasoracle&apikey=' + process.env["REACT_APP_PROXY_MULTISENDER_BSCSCAN_API_KEY"]
                 currencyTicker = 'BNB'
                 currencyTickerName = 'BNB'
                 blockchainName = 'Binance Smart Chain Test'
