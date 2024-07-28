8e32bf13610ee23e749f52b99f73df032be7a46b
Oleg Abrosimov <olegabrosimovnsk@gmail.com>
2024-07-28 13:59:21 +0700

    npm vulnerabilities warnings fix part 1

--- config-overrides.js
+++ config-overrides.js
@@ -4,16 +4,44 @@ const {
   fixBabelImports,
   addLessLoader,
   addDecoratorsLegacy,
+  disableEsLint,
 } = require("customize-cra");
 
+const webpack = require("webpack");
 
 module.exports = override(
   addDecoratorsLegacy(),
   fixBabelImports("import", {
-    libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true, // change importing css to less
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { "@primary-color": "#1DA57A" }
-  })
+    modifyVars: { "@primary-color": "#1DA57A" },
+  }),
+  function override(config) {
+    config.ignoreWarnings = [/Failed to parse source map/];
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      assert: require.resolve("assert"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify"),
+      url: require.resolve("url"),
+      zlib: require.resolve("browserify-zlib"),
+    });
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+        Buffer: ["buffer", "Buffer"],
+      }),
+    ]);
+    return config;
+  },
+  // disable eslint in webpack
+  disableEsLint()
 );
